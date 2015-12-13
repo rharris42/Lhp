e9429564b5b59e69e7e2cc1bdf3302359674d31a
rharris42 <robert.t.harris@gmail.com>
2015-12-13 12:58:47 -0800

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -1,6 +1,6 @@
 <?php
   /**
-   * Copyright 2014 Last Hit Productions, Inc.
+   * Copyright 2015 Last Hit Productions, Inc.
    *
    * You are hereby granted a non-exclusive, worldwide, royalty-free license to
    * use, copy, modify, and distribute this software in source code or binary
@@ -26,7 +26,115 @@
    * Lhp library auto loader
    * @author Robert Harris <robert.t.harris@gmail.com>
    */
-  require __DIR__ . '/lhp/lhp.php';
+  
+  /**
+   * Class Lhp - System control class
+   */
+  class Lhp {
+  
+    /** 
+     *  @var array - List of required classes 
+     */ 
+    private static $required_classes = array(
+      'LhpBrowser',
+      'LhpCookie',
+      'LhpDate',
+      'LhpFile',
+      'LhpForm',
+	  'LhpMail',
+      'LhpMysql',
+      'LhpRequest',
+      'LhpRoute',
+      'LhpTemplate',
+	  'LhpUser'
+	);
+  
+    /** 
+     *  load - Require *.php library files in given path 
+	 *
+	 * @throws exception
+     */ 
+	public static function Load($path) {
+	  // print "library dir = $path<BR>";
+	  if(is_readable($path)) {
+        foreach(glob("$path/*.php") as $file) {
+          if(!is_readable($file)) {
+	        throw new Exception("Unable to load library file: $file - File is not readable or does not exist.");
+	      }
+	      require $file;
+        }
+      }
+      else {
+        throw new Exception("Unable to load library directory: $path - Directory is not readable or does not exist.");
+      }
+	}
+	
+    /** 
+     *  CheckConfig - Checks lhp configuration files and make sure we have loaded all necessary classes
+	 *
+	 * @throws exception 
+     */ 
+	public static function CheckConfig() {
+	  foreach(static::$required_classes as $class) {
+        if(!class_exists($class, false)) {
+		  $file = strtolower(preg_replace('/^Lhp/', '', $class));
+		  throw new Exception("Failed to load required class: $class - File missing: " . __DIR__ . "/system/classes/$file.php");
+		}
+	  }
+	  if(!defined('DOMAIN_PATH')) {
+	    throw new Exception("Constant DOMAIN_PATH is not defined in  " . __DIR__ . "/system/lib/config.php");
+	  }
+	  if(!is_readable(DOMAIN_PATH)) {
+	    throw new Exception("DOMAIN_PATH does not exist or is not readable: " . DOMAIN_PATH);
+	  }
+	  if(!defined('TEMP_DIRECTORY')) {
+	    throw new Exception("Constant TEMP_DIRECTORY is not defined in  " . __DIR__ . "/system/lib/config.php");
+	  }
+	  if(!is_readable(TEMP_DIRECTORY)) {
+	    throw new Exception("TEMP_DIRECTORY does not exist or is not readable: " . TEMP_DIRECTORY);
+	  }
+	}
+	
+    /** 
+     *  CheckDomainConfig - Checks user domain configuration files and make sure we have loaded all necessary classes
+	 *
+	 * @throws exception 
+     */ 
+	public static function CheckDomainConfig() {
+      if(!defined('MYSQL_USER') || !defined('MYSQL_PASS') || !defined('MYSQL_DB') || !defined('MYSQL_HOST') || !defined('MYSQL_PORT')) {
+        throw new Exception("Please check the configuration file and make sure you have set the MYSQL_ constant definitions: " . DOMAIN_PATH . "/lib/config.php");
+      }
+	  if(!defined('TEMPLATE_DIR')) {
+	    throw new Exception("Constant TEMPLATE_DIR is not defined in " . DOMAIN_PATH . "/lib/config.php");
+	  }
+	  if(!is_readable(TEMPLATE_DIR)) {
+	    throw new Exception("TEMPLATE_DIR does not exist or is not readable: " . TEMPLATE_DIR);
+	  }
+	}
+  
+    /**
+     * error - prints exception error
+     */
+	public static function Debug($msg) {
+	  if(DEBUG) {
+	    print $msg;
+	    print CMD_LINE ? "\n" : "<BR>";
+	  }
+	}
+  
+    /**
+     * error - prints exception error
+     */
+	public static function ExceptionError($e) {
+	  if(DEBUG) {
+	    print "\n\nError: " . $e->getMessage() . "\n\n";
+	    exit;
+	  }
+	  else if(!CMD_LINE) {
+	    LhpBrowser::redirectToUrl('http://www.' . DOMAIN);
+	  }
+	}
+  }
   
   /**
    * We only use one main try block to capture all exceptions for a centralized error control handler
@@ -36,12 +144,12 @@
     /**
      * Load lhp classes
      */
-    Lhp::Load(__DIR__ . "/lhp/classes");
+    Lhp::Load(__DIR__ . "/system/classes");
   
     /**
      * Load lhp libraries
      */
-    Lhp::Load(__DIR__ . "/lhp/lib");
+    Lhp::Load(__DIR__ . "/system/lib");
 	
     /**
      * Check lhp configuration
@@ -111,10 +219,7 @@
      * then auto log in the user if possible  
      * 
      */
-    if($user->logged_in()) {
-      /* debugging */
-      Lhp::Debug("user is logged in");
-	  
+    if($user->logged_in()) {	  
       /* auto login user if we have valid session id */
       if(!$user->validate()) {
         $session_id = $user->getSessionHash();
@@ -125,7 +230,6 @@
 	      $uri = 'logout';
 	    }
 	  }
-	  
 	  /* refresh user data as needed */
 	  else if($user->validate() && $user->need_to_refresh() && !$user->update_session(UserRoute::$mysql_relogin_query . "users.id=" . $user->get('id') . " limit 1")) {
 	    $uri = 'logout';
@@ -156,6 +260,8 @@
     /** 
      * Route is based upon current request uri
      * Routing is executed by UserRoute::$route()
+	 * Uri is stripped of all characters except for letters and numbers to perform php function call
+	 * example: /some-page would execute the php function UserRoute::somepage()
      */
     $routing = $template->findRoute($uri);
     if($routing !== false && is_callable($routing)) {
