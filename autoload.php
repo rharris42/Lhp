5cd8325a5e3c784eeb0210f2ad9243ba835cb635
rharris42 <robert.t.harris@gmail.com>
2016-01-07 09:12:29 -0800

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -1,40 +1,21 @@
 <?php
   /**
    * Copyright 2015 Last Hit Productions, Inc.
-   *
-   * You are hereby granted a non-exclusive, worldwide, royalty-free license to
-   * use, copy, modify, and distribute this software in source code or binary
-   * form for use in connection with the web services and APIs provided by
-   * Last Hit Producions (LHP).
-   *
-   * As with any software that integrates with the LHP platform, your use
-   * of this software is subject to the LHP Developer Principles and
-   * Policies [http://developers.lhpdigital.com/policy/]. This copyright notice
-   * shall be included in all copies or substantial portions of the software.
-   *
-   * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-   * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-   * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-   * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-   * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-   * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-   * DEALINGS IN THE SOFTWARE.
-   *
    */
-   
+
   /**
    * Lhp library auto loader
    * @author Robert Harris <robert.t.harris@gmail.com>
    */
-  
+
   /**
    * Class Lhp - System control class
    */
   class Lhp {
-  
-    /** 
-     *  @var array - List of required classes 
-     */ 
+
+    /**
+     *  @var array - List of required classes
+     */
     private static $required_classes = array(
       'LhpBrowser',
       'LhpCookie',
@@ -48,12 +29,12 @@
       'LhpTemplate',
 	  'LhpUser'
 	);
-  
-    /** 
-     *  load - Require *.php library files in given path 
+
+    /**
+     *  load - Require *.php library files in given path
 	 *
 	 * @throws exception
-     */ 
+     */
 	public static function Load($path) {
 	  // print "library dir = $path<BR>";
 	  if(is_readable($path)) {
@@ -68,12 +49,12 @@
         throw new Exception("Unable to load library directory: $path - Directory is not readable or does not exist.");
       }
 	}
-	
-    /** 
+
+    /**
      *  CheckConfig - Checks lhp configuration files and make sure we have loaded all necessary classes
 	 *
-	 * @throws exception 
-     */ 
+	 * @throws exception
+     */
 	public static function CheckConfig() {
 	  foreach(static::$required_classes as $class) {
         if(!class_exists($class, false)) {
@@ -94,12 +75,12 @@
 	    throw new Exception("TEMP_DIRECTORY does not exist or is not readable: " . TEMP_DIRECTORY);
 	  }
 	}
-	
-    /** 
+
+    /**
      *  CheckDomainConfig - Checks user domain configuration files and make sure we have loaded all necessary classes
 	 *
-	 * @throws exception 
-     */ 
+	 * @throws exception
+     */
 	public static function CheckDomainConfig() {
       if(!defined('MYSQL_USER') || !defined('MYSQL_PASS') || !defined('MYSQL_DB') || !defined('MYSQL_HOST') || !defined('MYSQL_PORT')) {
         throw new Exception("Please check the configuration file and make sure you have set the MYSQL_ constant definitions: " . DOMAIN_PATH . "/lib/config.php");
@@ -111,7 +92,7 @@
 	    throw new Exception("TEMPLATE_DIR does not exist or is not readable: " . TEMPLATE_DIR);
 	  }
 	}
-  
+
     /**
      * error - prints exception error
      */
@@ -121,7 +102,7 @@
 	    print CMD_LINE ? "\n" : "<BR>";
 	  }
 	}
-  
+
     /**
      * error - prints exception error
      */
@@ -135,65 +116,66 @@
 	  }
 	}
   }
-  
+
   /**
    * We only use one main try block to capture all exceptions for a centralized error control handler
    */
   try {
-   
+
     /**
      * Load lhp classes
      */
     Lhp::Load(__DIR__ . "/classes");
-  
+
     /**
      * Load lhp libraries
      */
     Lhp::Load(__DIR__ . "/lib");
-	
+
     /**
      * Check lhp configuration
      */
     Lhp::CheckConfig();
-  
+
     /**
      * Load domain specific classes
      */
     Lhp::Load(DOMAIN_PATH . "/classes");
-   
+
     /**
      * Load domain specific libraries
      */
     Lhp::Load(DOMAIN_PATH . "/lib");
-	
+
     /**
      * Check domain specific configuration
      */
     Lhp::CheckDomainConfig();
-	
+
     /**
-     * Check to make sure we are on www 
+     * Check to make sure we are on www
      */
-    (LhpBrowser::getServerName() == 'www.' . DOMAIN) or
-      LhpBrowser::redirectToUrl('http' . (LhpBrowser::isSecure()?'s':'') . '://www.' . DOMAIN . LhpBrowser::getRequestUri(), 404);
-	
+    if(LhpBrowser::getServerName() == DOMAIN) {
+      LhpBrowser::redirectToUrl('http' . (LhpBrowser::isSecure()?'s':'') . '://www.' . DOMAIN . LhpBrowser::getRequestUri(), 301);
+	}
+
     /**
      * Initiate LhpForm with filtered form data (true)
      * Set default form values
      */
     $form = new LhpForm(true);
-    
+
     /**
      * Initiate LhpCookie for use with current domain and all sub domains (ie '.domain.com')
      */
     $cookie = new LhpCookie('.' . DOMAIN);
-    
+
     /**
-     * Initiate LhpMysql - To save resources, we connect to the mysql server 
+     * Initiate LhpMysql - To save resources, we connect to the mysql server
      * only when a query is executed, not on every request
      */
     $mysql = new LhpMysql(MYSQL_USER, MYSQL_PASS, MYSQL_DB, MYSQL_HOST, MYSQL_PORT);
-    
+
     /**
      * Load user data using sessions
 	 * Set default session data
@@ -201,51 +183,48 @@
     $user = new LhpUser($cookie, $mysql);
     $user->set_default('id', 1);
     $user->set_default('role', 0);
-	
+
     /**
      * Initiate template object.
      * Set default template placeholder values
      */
     $template = new LhpTemplate();
-	
+
     /**
      * Set URI variable for routing
      */
     $uri = LhpBrowser::getRequestUri();
 	$uri = preg_replace('/\?.+$/', '', $uri);
-	
+
     /**
      * check to see if we have logged in cookie hash but no session
-     * then auto log in the user if possible  
-     * 
+     * then auto log in the user if possible
+     *   auto login user if we have valid session id or refresh user data as needed
      */
-    if($user->logged_in()) {	  
-      /* auto login user if we have valid session id */
+    if($user->logged_in()) {
+	  $session_id = $user->getSessionHash();
       if(!$user->validate()) {
-        $session_id = $user->getSessionHash();
-        if($user->login(UserRoute::$mysql_relogin_query . "users.session_id='" . $mysql->real_escape($session_id) . "' limit 1")) {
-	      $user->setBypassValidation(true);
-	    }
-	    else {
+	    if(!$user->login(array('session_id' => $session_id))) {
 	      $uri = 'logout';
-	    }
+		}
 	  }
-	  /* refresh user data as needed */
-	  else if($user->validate() && $user->need_to_refresh() && !$user->update_session(UserRoute::$mysql_relogin_query . "users.id=" . $user->get('id') . " limit 1")) {
+	  else if($user->validate() && $user->need_to_refresh() && !$user->update_session($user->get('id'))) {
+	    //print "trying to refresh user session";
+		//exit;
 	    $uri = 'logout';
 	  }
     }
-	
+
     /**
      * Set or check form tokens depending on form action
      */
-	if($form->get('action') == 'save' && !$user->getBypass() && !check_form_token($form, $cookie, $user)) {
+	if(!$form->check_tokens($cookie, $user)) {
 	  throw new Exception("Form hash tokens do not match!");
 	}
 	else {
-	  set_form_token($template);
+	  $form->set_tokens($template);
 	}
-   
+
     /**
      * Load domain specific executables
 	 * Files are loaded in alphabetical order
@@ -256,8 +235,8 @@
 	  }
 	  require $file;
     }
-    
-    /** 
+
+    /**
      * Route is based upon current request uri
      * Routing is executed by UserRoute::$route()
 	 * Uri is stripped of all characters except for letters and numbers to perform php function call
@@ -270,29 +249,15 @@
 	else {
 	  Lhp::Debug("NO ROUTING FUNCTION FOUND");
 	}
-	
+
     /**
-     * Redirect to URI if applicable
+     * Redirect to URL if applicable
      */
 	if($template->getRedirect() !== null) {
 	  $mysql->disconnect();
 	  LhpBrowser::redirectToUrl($template->getRedirect(), 302);
 	}
-	
-    /**
-     * Further debugging information
-	 * Cookies, session cookie values
-     */
-	if(DEBUG) {
-	  //foreach($_COOKIE as $key=>$val) {
-	  //  print "$key = $val<BR>";
-	  //}
-	  //$info = session_get_cookie_params();
-	  //foreach($info as $key=>$val) {
-	  //  print "$key = $val<BR>";
-	  //}
-	}
-    
+
     /**
      * Load template file, parse template placeholders and print to screen
      * Template file is based upon current request uri (route) (/somepage.html or /somepage = somepage.tpl.htm)
@@ -303,12 +268,12 @@
       $template->parseAll();
       $template->show();
 	}
-    
+
     /**
      * Disconnect from mysql as needed
      */
     $mysql->disconnect();
-	
+
   }
   catch (Exception $e) {
     Lhp::ExceptionError($e);
