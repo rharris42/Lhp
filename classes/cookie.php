8457055bacb0d645bade93a4ba40539b9daafa7b
rharris42 <robert.t.harris@gmail.com>
2016-01-07 09:15:06 -0800

    Update cookie.php

--- classes/cookie.php
+++ classes/cookie.php
@@ -1,47 +1,28 @@
 <?php
   /**
    * Copyright 2015 Last Hit Productions, Inc.
-   *
-   * You are hereby granted a non-exclusive, worldwide, royalty-free license to
-   * use, copy, modify, and distribute this software in source code or binary
-   * form for use in connection with the web services and APIs provided by
-   * Last Hit Producions (LHP).
-   *
-   * As with any software that integrates with the LHP platform, your use
-   * of this software is subject to the LHP Developer Principles and
-   * Policies [http://developers.lhpdigital.com/policy/]. This copyright notice
-   * shall be included in all copies or substantial portions of the software.
-   *
-   * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-   * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-   * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-   * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-   * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-   * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-   * DEALINGS IN THE SOFTWARE.
-   *
    */
-   
+
   /**
    * Class LhpCookie
    * @author Robert Harris <robert.t.harris@gmail.com>
    */
   class LhpCookie {
-  
+
     /**
      * @var string - Path of cookie
      */
 	private $path;
-	
+
     /**
      * @var string - Domain of cookie
      */
 	private $domain;
-	
+
     /**
      * LhpCookie - Returns cookie object to handle reading, writing and deleting cookies
      *  cookies can be created upon object creating using $cookies array consisting of name, value, expiration
-	 * 
+	 *
      * @param string|null $path
      * @param string $path
      * @param array $cookies
@@ -54,7 +35,7 @@
 	    $this->set($cookie[0],$cookie[1],$days);
 	  }
     }
-	
+
     /**
      * fields - Returns array of all keys found in $_COOKIE
 	 *
@@ -63,7 +44,7 @@
 	public function fields() {
 	  return array_keys($_COOKIE);
     }
-	
+
     /**
      * get - Returns cookie value of given $name if available
 	 *
@@ -72,9 +53,9 @@
 	 * @return string:null
      */
 	public function get($name) {
-      return isset($_COOKIE[$name]) ? $_COOKIE[$name] : null; 
+      return isset($_COOKIE[$name]) ? $_COOKIE[$name] : null;
     }
-	
+
     /**
      * set - Sets cookie $name based on value $value with an expiration of time() + $days
 	 *
@@ -93,9 +74,9 @@
 	  }
 	  return $this;
     }
-	
+
     /**
-     * delete - Sets cookie for deletion using given arguments 
+     * delete - Sets cookie for deletion using given arguments
 	 *
 	 * @param string $name
 	 * @param string $name2
@@ -110,6 +91,6 @@
 	  }
 	  return $this;
     }
-	
+
   }
 ?>
